419513a32a6dcd2127d45c44d353d08332192117
Maya Gabbay <maya.gabbay@post.runi.ac.il>
2024-11-09 12:01:24 +0200

    doneHomeWork

--- FVCalc.java
+++ FVCalc.java
@@ -2,13 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
-        int rate = Integer.parseInt(args[1]);
+        double rate = Double.parseDouble(args[1]);
         double precentage = (double)rate/100;
-        System.out.println(precentage);
         int years = Integer.parseInt(args[2]);
         double ratePrecentage = Math.pow(1+precentage,years);
         double futureValue = currentValue*ratePrecentage;
         System.out.println("After " + years + " years, a $"
-		 + currentValue + " saved at " + (double)rate +"%" + " will yield $" + (int)futureValue);	
+		 + currentValue + " saved at " + rate +"%" + " will yield $" + (int)futureValue);	
 		}
 }

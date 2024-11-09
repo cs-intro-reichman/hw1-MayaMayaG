7d965ec9eaf97221d117b65607dc528f5c05136d
Maya Gabbay <maya.gabbay@post.runi.ac.il>
2024-11-09 11:52:48 +0200

    doneHomeWork

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
         String name3 = args[2];
         double totalPrice = Double.parseDouble(args[3]);
         double payPrice = totalPrice/3;
-        System.out.println("Dear " + name3 +", " + name2 + ", " + name1 +
+        System.out.println("Dear " + name3 +", " + name2 + ", and " + name1 +
 		 ": pay " + Math.ceil(payPrice) + " Shekels each");
 	}
 
